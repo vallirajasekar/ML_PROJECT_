13b4cb762fe019b0b4ea57eb5630b903188693a9
Valli raja  Sekar <vallirajasekar@Vallis-MacBook-Air.local>
2025-03-12 11:17:41 +0530

    Setup and Requirements.txt

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+
+def get_requirements(file_path: str) -> List[str]:
+    """
+    This function will return the list of requirements.
+    """
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.strip() for req in requirements]  # Using strip() to remove newline characters
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
+setup(
+    name='ML_PROJECT',
+    version='0.0.1',
+    author='Valli Raja Sekar',  # Missing comma fixed here
+    author_email='sekarvalliraja@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
+
+
